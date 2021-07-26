602dd3c29ac06b5170fd9797ba0038a6d54ab7af
mikiimikii <hajorge2012@gmail.com>
2021-07-26 15:39:03 -0300

    fix size sections

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 const menuIcon = document.querySelector('.nav_btn_ham');
 const navBar = document.querySelector('.nav_menu_hamburguer');
+const navBarLink = document.querySelector('.nav_link');
+
 
 menuIcon.addEventListener('click', () => {
+  navBar.classList.toggle('change');
+});
+
+navBarLink.addEventListener('click', () => {
   navBar.classList.toggle('change');
 });
